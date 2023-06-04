6eb6e0fdce5306023e28e99c5c0ad3e8f2e34955
AAstahckin <ALVAstashkin91@yandex.ru>
2023-06-05 01:24:20 +0300

    fix

--- src/test/java/pages/components/MarketingPopupComponents.java
+++ src/test/java/pages/components/MarketingPopupComponents.java
@@ -1,13 +1,27 @@
 package pages.components;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.Cookie;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
 public class MarketingPopupComponents {
 
+    SelenideElement confirmButton = $("#confirm-button");
+
     public void shutdownMarketingPopup() {
-        Cookie cookiePopupsShownCount = new Cookie("popups-popup-pdd-spring-shown-count", "1");
-        WebDriverRunner.getWebDriver().manage().addCookie(cookiePopupsShownCount);
+        if(confirmButton.isDisplayed()) {
+            confirmButton.click();
+            Cookie cookiePopupsShownCount = new Cookie("popups-popup-pdd-spring-shown-count", "1");
+            WebDriverRunner.getWebDriver().manage().addCookie(cookiePopupsShownCount);
+        } else {
+            Cookie cookiePopupsShownCount = new Cookie("popups-popup-pdd-spring-shown-count", "1");
+            WebDriverRunner.getWebDriver().manage().addCookie(cookiePopupsShownCount);
+        }
     }
 
 }
